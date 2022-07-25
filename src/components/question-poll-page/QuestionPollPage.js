85e05df0320875d2a405845d0c7288cde80caa11
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2022-07-25 13:05:46 +0200

    refactor: changing router link to button

--- src/components/question-poll-page/QuestionPollPage.js
+++ src/components/question-poll-page/QuestionPollPage.js
@@ -1,24 +1,34 @@
 import { connect } from 'react-redux';
 import './question-poll-page.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const QuestionPollPage = (props) => {
+  const navigate = useNavigate();
+  const handleAnswer = (e) => {
+    console.log(e.target.id);
+    navigate('/');
+  };
+
   return (
     <div className="question-poll-question-poll">
       {'This is Question: ' + props.question.id}
       <div>Choose one !</div>
       <div className="answer-wrapper">
-        <Link to={'/'}>
-          <div className="question-poll-option-one">
-            {props.question.optionOne.text}
-          </div>
-        </Link>
-        <Link to={'/'}>
-          <div className="question-poll-option-two">
-            {props.question.optionTwo.text}
-          </div>
-        </Link>
+        <button
+          id="answer1"
+          onClick={handleAnswer}
+          className="question-poll-option-one"
+        >
+          {props.question.optionOne.text}
+        </button>
+        <button
+          id="answer2"
+          onClick={handleAnswer}
+          className="question-poll-option-two"
+        >
+          {props.question.optionTwo.text}
+        </button>
       </div>
     </div>
   );
