c4f96cb3033b460324734d75d3237880aa608a28
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2022-07-25 12:14:06 +0200

    refactor: improving leaderboard

--- src/components/leader-board/LeaderBoard.js
+++ src/components/leader-board/LeaderBoard.js
@@ -1,29 +1,55 @@
 import { connect } from 'react-redux';
 import users from '../../reducers/users';
+import { Link } from 'react-router-dom';
 
 const LeaderBoard = (props) => {
-  const getUserNames = () => {
-    console.log(props.users);
-    props.users.map((user) => console.log(user));
-    props.users.map((user) => console.log(Object.values(user.answers).length));
-    props.users.map((user) =>
-      console.log(Object.values(user.questions).length)
-    );
+  const getUserLeadershipData = () => {
+    props.users.map((user) => {
+      if (user.id === props.authedUser) {
+        console.log(Object.values(user.answers).length);
+      }
+    });
   };
 
   return (
-    <div
-      style={{ width: '600px', border: '1px solid black', overflow: 'hidden' }}
-    >
-      {props.users.map((user) => (
-        <div>{user.id}</div>
-      ))}
-      {props.users.map((user) => (
-        <div>{Object.values(user.answers).length}</div>
-      ))}
-      {props.users.map((user) => (
-        <div>{Object.values(user.questions).length}</div>
-      ))}
+    <div>
+      <div
+        style={{
+          width: '600px',
+          border: '1px solid black',
+          overflow: 'hidden',
+        }}
+      >
+        {props.users.map((user) => (
+          <div>{user.id === props.authedUser ? user.id : null}</div>
+        ))}
+        {props.users.map((user) => (
+          <div>
+            {user.id === props.authedUser
+              ? Object.values(user.answers).length
+              : null}
+          </div>
+        ))}
+        {props.users.map((user) => (
+          <div>
+            {user.id === props.authedUser
+              ? Object.values(user.questions).length
+              : null}
+          </div>
+        ))}
+        {props.users.map((user) => (
+          <div>{user.id}</div>
+        ))}
+        {props.users.map((user) => (
+          <div>{Object.values(user.answers).length}</div>
+        ))}
+        {props.users.map((user) => (
+          <div>{Object.values(user.questions).length}</div>
+        ))}
+      </div>
+      <Link to={'/'}>
+        <div className="question-poll-option-two">Home</div>
+      </Link>
     </div>
   );
 };
