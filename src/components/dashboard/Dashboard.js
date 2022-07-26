6e77040dd9ee0216e036df952a786d29fcce6952
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2022-07-26 17:01:51 +0200

    feat: adding switch for toggling dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,8 +3,10 @@ import QuestionCard from '../question-card/QuestionCard';
 import GridList from '@material-ui/core/ImageList';
 import './dashboard.css';
 import Navbar from '../navbar/Navbar';
+import { useState } from 'react';
 
 const Dashboard = ({ questions, questionIDs, authedUser }) => {
+  const [switchAnsweredQuestions, setSwitchAnsweredQuestions] = useState(false);
   const checkForAlreadyAnsweredQuestions = (question) => {
     const questionOptionOneVotes = question.optionOne.votes;
     const questionOptionTwoVotes = question.optionTwo.votes;
@@ -25,35 +27,56 @@ const Dashboard = ({ questions, questionIDs, authedUser }) => {
     return alreadyAnswered;
   };
 
+  const handleChange = (e) => {
+    console.log(e.target.checked);
+    setSwitchAnsweredQuestions(e.target.checked);
+  };
+
   return (
     <div>
       <Navbar></Navbar>
+      <label className="switch">
+        <label className="switch-unanswered-label">Unanswered</label>
+        <input type="checkbox" onChange={handleChange}></input>
+        <span className="slider"></span>
+        <label className="switch-answered-label">Answered</label>
+      </label>
       <h1 style={{ textAlign: 'center' }}>
-        Your Question Dashboard,{' '}
-        {String(authedUser)[0].toUpperCase() + String(authedUser).slice(1)}
+        {' Your Question Dashboard, ' +
+          String(authedUser)[0].toUpperCase() +
+          String(authedUser).slice(1)}
       </h1>
-      <h2 style={{ paddingTop: '20px', paddingLeft: '20px' }}>Unanswered</h2>
-      <GridList className="dashboard-list-new">
-        {questionIDs.map((id) =>
-          checkForAlreadyAnsweredQuestions(questions[id]) ? null : (
-            <QuestionCard questionID={id} key={'card' + id}></QuestionCard>
-          )
-        )}
-      </GridList>
-      <h2 style={{ paddingTop: '20px', paddingLeft: '20px' }}>
-        Already Answered
-      </h2>
-      <GridList className="dashboard-list-old">
-        {questionIDs.map((id) =>
-          checkForAlreadyAnsweredQuestions(questions[id]) ? (
-            <QuestionCard
-              questionID={id}
-              key={'card' + id}
-              answered
-            ></QuestionCard>
-          ) : null
-        )}
-      </GridList>
+      {switchAnsweredQuestions ? (
+        <div>
+          <h2 style={{ paddingTop: '20px', paddingLeft: '20px' }}>
+            Unanswered
+          </h2>
+          <GridList className="dashboard-list-new">
+            {questionIDs.map((id) =>
+              checkForAlreadyAnsweredQuestions(questions[id]) ? null : (
+                <QuestionCard questionID={id} key={'card' + id}></QuestionCard>
+              )
+            )}
+          </GridList>
+        </div>
+      ) : (
+        <div>
+          <h2 style={{ paddingTop: '20px', paddingLeft: '20px' }}>
+            Already Answered
+          </h2>
+          <GridList className="dashboard-list-old">
+            {questionIDs.map((id) =>
+              checkForAlreadyAnsweredQuestions(questions[id]) ? (
+                <QuestionCard
+                  questionID={id}
+                  key={'card' + id}
+                  answered
+                ></QuestionCard>
+              ) : null
+            )}
+          </GridList>
+        </div>
+      )}
     </div>
   );
 };
