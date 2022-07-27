5dfc743f6eecf6c40a682798e3a36957e24af27a
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2022-07-27 02:32:30 +0200

    fix: linting issues

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -14,14 +14,18 @@ const Dashboard = ({ questions, questionIDs, authedUser }) => {
     let alreadyAnswered = false;
 
     questionOptionOneVotes.map((votingName) => {
-      votingName === loggedInUserName
-        ? (alreadyAnswered = true)
-        : (alreadyAnswered = alreadyAnswered);
+      if (votingName === loggedInUserName) {
+        alreadyAnswered = true;
+        return alreadyAnswered;
+      }
+      return alreadyAnswered;
     });
     questionOptionTwoVotes.map((votingName) => {
-      votingName === loggedInUserName
-        ? (alreadyAnswered = true)
-        : (alreadyAnswered = alreadyAnswered);
+      if (votingName === loggedInUserName) {
+        alreadyAnswered = true;
+        return alreadyAnswered;
+      }
+      return alreadyAnswered;
     });
 
     return alreadyAnswered;
